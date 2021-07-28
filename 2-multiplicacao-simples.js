a79d21a786081dfdf9957cfa075020f34de877e7
joelfirmino <joelfirmino@gmail.com>
2021-07-28 18:36:53 -0300

    desafio programacao java script

---
+++ 2-multiplicacao-simples.js
@@ -0,0 +1,10 @@
+/**
+ * 
+Desafio- DIO - 02
+
+*/
+
+let valor1 = parseInt(gets());
+let valor2 = parseInt(gets());
+let total = valor1 * valor2; // Altere o valor da variável com o cálculo esperado 
+console.log("PROD = " + total);

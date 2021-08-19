f95bcbf8298e5112e73c6c091ff16c58acc4a298
joefirmino <49295666+joefirmino@users.noreply.github.com>
2021-08-19 11:12:15 -0300

    Add files via upload

---
+++ Java/Iniciando a programação em Java/3.DiscagemInterurbana.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+import java.util.Locale;
+
+public class EntradaCPF{
+
+	public static void main(String[] args) {
+		
+		Locale.setDefault(new Locale("en", "US"));
+		
+		Scanner sc = new Scanner(System.in);
+		String[] cpf = new String[4]; 
+		
+		cpf = sc.nextLine().split("-");
+		
+		for(int i= 0; i < cpf.length; i++) {
+		  System.out.println(cpf[i].replace(".","\n"));
+	  }
+	  
+		sc.close();
+	}
+}
